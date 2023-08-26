a78801822b18ce27c0143f43a15978027a8adafd
Benjamin <benjamin.traub@bmw.de>
2023-08-26 13:07:42 +0200

    merge conflicts fixed

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -10,7 +10,6 @@ class LoadFactOperator(BaseOperator):
     def __init__(self,
                  redshift_conn_id='',
                  table='',
-<<<<<<< HEAD
                  target_sql='',
                  *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -31,17 +30,3 @@ class LoadFactOperator(BaseOperator):
         self.log.info(f"Running SQL command: {insert_sql}")
         postgres_hook.run(insert_sql)
         self.log.info(f"SQL command: {insert_sql} completed")
-=======
-                 sql='',
-                 *args, **kwargs):
-        super(LoadFactOperator, self).__init__(*args, **kwargs)
-        self.redshift_conn_id = redshift_conn_id
-        self.table = table
-        self.sql = sql
-
-    def execute(self, context):
-        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-        self.log.info(f'Loading data into {self.table} fact table...')
-        redshift_hook.run(self.sql)
-        self.log.info(f'Data loaded into {self.table} fact table.')
->>>>>>> a872af15fe612615fba9087019550d80c84efa8e
